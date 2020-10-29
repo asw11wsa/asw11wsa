560309c4d8d103d5ef421f74e35604fd2683be71
asw11wsa <asw11wsa@naver.com>
2020-10-29 09:02:47 +0900

    change write_process

--- write_process.php
+++ write_process.php
@@ -3,7 +3,7 @@
   $sql = "INSERT INTO `topic` (`title`,`description`,`created`) VALUES ('".$_POST['title']."','".$_POST['description']."',NOW())";
   $result = mysqli_query($conn,$sql);
   if($result){
-      header("Location:study2.php?id='".$_POST['title']."'");
+      header("Location:study2.php?id=".$_POST['title']."");
   }else{
       echo "no writing";
   }
