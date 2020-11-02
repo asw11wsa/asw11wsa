533cf12a11f19a7fa1cec1f41c0948e8db272486
asw11wsa <asw11wsa@naver.com>
2020-11-02 18:10:09 +0900

    update security

--- project/deleteProcess.php
+++ project/deleteProcess.php
@@ -1,6 +1,7 @@
 <?php
     require_once $_SERVER['DOCUMENT_ROOT']."/dbconnect.php";
-    $sql = "DELETE FROM `topic` WHERE `id` = '".$_POST['id']."'";
+    $filtered_id = mysqli_real_escape_string($conn,$_POST['id']);
+    $sql = "DELETE FROM `topic` WHERE `id` = {$filtered_id}";
     $result = mysqli_query($conn,$sql);
     if($result){
         header('Location:/project/design.php');
