164899839659b9e9f84e3844da82ad51180d929c
asw11wsa <asw11wsa@naver.com>
2020-11-13 16:41:04 +0900

    write HTML about page description

---
+++ design/index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/asset/css/default.css">
+    <link rel="stylesheet" type="text/css" href="/asset/css/main.css">
+    <title>SION'S STUDY</title>
+</head>
+<body>
+    <div class="container">
+        <nav class="global-nav">
+            <div class="global-nav-links">
+                <a href="#" class="global-nav-item">Rooms</a>
+                <a href="#" class="global-nav-item">Ideas</a>
+                <a href="#" class="global-nav-item">Stores</a>
+                <a href="#" class="global-nav-item">Contact</a>
+            </div>
+        </nav>
+        <nav class="local-nav">
+            <div class="local-nav-links">
+                <a href="" class="product-name">AirMug Pro</a>
+                <a href="#">개요</a>
+                <a href="#">제품사양</a>
+                <a href="#">구입하기</a>
+            </div>
+        </nav>
+        <section class="scroll-section" id="scroll-section-0">
+            <h1>AriMug Pro</h1>
+            <div class="sticky-elem main-message">
+                <p>온전히 빠져들게 하는<br>최고급 세라믹</p>
+            </div>
+            <div class="sticky-elem main-message">
+                <p>주변의 맛을 느끼게 해주는<br>주변 맛 허용 모드</p>
+            </div>
+            <div class="sticky-elem main-message">
+                <p>온종일 편안한<br>맞춤형 손잡이</p>
+            </div>
+            <div class="sticky-elem main-message">
+                <p>새로게 입가를<br>찾아온 매혹</p>
+            </div>
+        </section>
+        <section class="scroll-section" id="scroll-section-1">
+            <p>
+                <strong>보통 스크롤 영역</strong>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, at consequatur consequuntur culpa deserunt distinctio dolore ducimus ea enim excepturi fugiat illum ipsa iste itaque odio provident quae quam saepe temporibus ut velit veritatis voluptatem, voluptates voluptatibus voluptatum! Cum eum eveniet pariatur. Ad aliquam architecto blanditiis commodi consequatur culpa delectus dignissimos distinctio dolorum earum eius eos error esse est facere harum maxime nam neque nobis, numquam obcaecati perspiciatis placeat praesentium quaerat quidem quos recusandae reiciendis saepe sequi sit sunt veniam voluptates voluptatibus! Aliquam at atque corporis deleniti distinctio ex, hic impedit, iure maxime natus nostrum nulla praesentium, quam quasi quo temporibus tenetur voluptatibus! Ab ad aliquid amet animi asperiores aut, beatae commodi delectus deserunt enim et expedita ipsam ipsum magnam molestias nisi non nulla perferendis placeat praesentium quas sed sint veniam! Amet atque dolores eaque eius est expedita explicabo fugit in, iste laborum minima natus nesciunt nobis nulla pariatur qui ratione veniam, voluptatem. Blanditiis nisi obcaecati odit quis. Accusamus consectetur debitis doloremque, eum fuga iure iusto minima nesciunt nulla, numquam omnis perspiciatis quam ratione sint soluta tempore, voluptatum. Aliquam, asperiores aspernatur, cum cumque dolorem hic molestiae molestias nostrum optio quibusdam reiciendis repellendus! At autem id quidem reprehenderit veniam voluptatem. Nobis!
+            </p>
+        </section>
+        <section class="scroll-section" id="scroll-section-2">
+            <div class="sticky-elem main-message">
+                <p>
+                    <small>편안한 촉감</small>
+                    입과 하나 되다
+                </p>
+            </div>
+            <div class="sticky-elem desc-message">
+                <p>
+                    편안한 목넘김을 완성하는 디테일한 여러 구성 요서들, 우리는 이를 하나하나 새롭게 살피고 재구성하는 과정을 거쳐 새로운 수준의 머그, AriMug Pro를 만들었습니다. 입에 뭔가 댔다는 감각은 어느새 사라지고 오롯이 당신과 음료만 남게 되죠.
+                </p>
+                <div class="pin"></div>
+            </div>
+            <div class="sticky-elem main-message">
+                <p>
+                    디자인 앤 퀄리티 오브 스웨덴, <br> 메이드 인 차이나
+                </p>
+                <div class="pin"></div>
+            </div>
+        </section>
+        <section class="scroll-section" id="scroll-section-3">
+            <p class="mid-message">
+                <strong>Retina 머그</strong><br>
+                아이디어를 광활하게 펼칠<br>
+                아름답고 부드러운 음료 공간.
+            </p>
+            <p class="canvas-caption">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti iste libero molestiae perferendis possimus provident reiciendis tempore. Corporis eaque eum neque quas temporibus totam? Accusantium, ad adipisci aliquam aspernatur autem dolor dolores eaque fugiat illum ipsa itaque maiores nemo numquam odio officia quae quas quasi, quidem quod reiciendis sed sequi tempora, ullam. Aliquam aliquid, asperiores aspernatur culpa deleniti doloribus dolorum eaque esse est facere facilis fugit illo impedit laborum magnam maxime minima modi molestiae nam natus nesciunt nisi nobis non nulla obcaecati perspiciatis praesentium quam quibusdam quidem quis quod rem repellendus sit temporibus ut velit vitae. Accusamus quidem voluptate voluptatibus.
+            </p>
+        </section>
+        <footer class="footer">2020 By SION</footer>
+    </div>
+</body>
+</html>
