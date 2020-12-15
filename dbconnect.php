5762cba9040b46c8491530e812a0a87147d2ade5
asw11wsa <asw11wsa@naver.com>
2020-12-15 13:17:28 +0900

    remove direct database connect

--- dbconnect.php
+++ dbconnect.php
@@ -1,16 +1,9 @@
 <?php
-##### 데이터베이스 연결설정 인자 (서버명, 사용자명, 비밀번호, 데이터베이스명)
-$host= "localhost";
-$user= "asw11wsa";
-$password= "tjdghk5328";
-$db= "asw11wsa";
-
-##### 데이터베이스에 연결한다.
-$conn = @mysqli_connect($host,$user,$password,$db);
-if(!$conn) {
-    error("데이터베이스 연결실패");
-    exit;
-}
+include_once 'lib/home/database_connection.php';
+include_once 'lib/home/user_connection.php';
+include_once 'lib/home/view_user.php';
+include_once 'lib/home/topic_connect.php';
+include_once 'lib/home/view_topic.php';
 ?>
 
 
