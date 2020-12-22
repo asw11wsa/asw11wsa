436349967b07f1d7cf30e7684cd3bd1a5b46b557
asw11wsa <asw11wsa@naver.com>
2020-12-22 10:38:59 +0900

    change class-autoload depend url

--- include/class-autoload.inc.php
+++ include/class-autoload.inc.php
@@ -2,16 +2,19 @@
 spl_autoload_register('myAutoLoader');
 
 function myAutoLoader($className){
-    $path = "classes/";
-    $path2 = "$className/";
-    $extension = ".class.php";
-    $fullPath = $path . $path2 . $className . $extension;
+    $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
-    if(!file_exists($fullPath)){
-        return false;
+    if (strpos($url, 'includes') !== false){
+        $path = '../classes/';
+    }else{
+        $path = 'classes/';
     }
-
-//    echo $fullPath;
-    include_once $fullPath;
+    $extension = '.class.php';
+    require_once $path . $className . $extension;
 }
+
+// Check url
+//echo $_SERVER['HTTP_HOST'];
+//echo "<br>";
+//echo $_SERVER['REQUEST_URI'];
 
