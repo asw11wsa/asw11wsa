1bd8bafd016a140a16220512c10500ffa2ec4f30
SIULEE <siudevelop@gmail.com>
2021-01-07 09:15:42 +0900

    20201220 study

--- classes/Calc.class.php
+++ classes/Calc.class.php
@@ -1,7 +1,8 @@
 <<<<<<< HEAD
 <?php
 
-class Calc {
+class Calc
+{
     public $operator;
     public $num1;
     public $num2;
@@ -20,7 +21,8 @@ class Calc {
         echo $this->num2;
     }
 
-    public function calculator(){
+    public function calculator()
+    {
         switch ($this->operator) {
             case 'add':
                 return $this->num1 + $this->num2;
@@ -39,46 +41,4 @@ class Calc {
                 break;
         }
     }
-=======
-<?php
-
-class Calc {
-    public $operator;
-    public $num1;
-    public $num2;
-
-    public function __construct($operator, $num1, $num2)
-    {
-        $this->operator = $operator;
-        $this->num1 = $num1;
-        $this->num2 = $num2;
-    }
-
-    public function getAllInfo()
-    {
-        echo $this->operator;
-        echo $this->num1;
-        echo $this->num2;
-    }
-
-    public function calculator(){
-        switch ($this->operator) {
-            case 'add':
-                return $this->num1 + $this->num2;
-                break;
-            case 'sub':
-                return $this->num1 - $this->num2;
-                break;
-            case 'div':
-                return $this->num1 / $this->num2;
-                break;
-            case 'mul':
-                return $this->num1 * $this->num2;
-                break;
-            default:
-                return "error";
-                break;
-        }
-    }
->>>>>>> origin/master
 }
--- dbtest.php
+++ dbtest.php
@@ -11,18 +11,3 @@ if($conn){
 $query = "SELECT * FROM `topic`";
 $result = mysqli_query($conn,$query);
 $num = mysqli_num_rows($result);
-=======
-<?php
-$conn = mysqli_connect("localhost","asw11wsa","Siu2034!","asw11wsa");
-
-if($conn){
-    echo "데이터베이스 연결성공";
-}else{
-    echo "데이터베이스 연결 실패";
-}
-
-$query = "SELECT * FROM `topic`";
-$result = mysqli_query($conn,$query);
-$num = mysqli_num_rows($result);
->>>>>>> origin/master
-echo $num['title'];
