b7b55872ea4063f31f14dd5f3040a1f10294fdd7
SIULEE <siudevelop@gmail.com>
2021-01-07 09:13:45 +0900

    20201220 study

---
+++ dbtest.php
@@ -0,0 +1,13 @@
+<?php
+$conn = mysqli_connect("localhost","asw11wsa","Siu2034!","asw11wsa");
+
+if($conn){
+    echo "데이터베이스 연결성공";
+}else{
+    echo "데이터베이스 연결 실패";
+}
+
+$query = "SELECT * FROM `topic`";
+$result = mysqli_query($conn,$query);
+$num = mysqli_num_rows($result);
+echo $num['title'];
